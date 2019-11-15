ae97cf82c8da2dd58b429a79f3f9b7fbbade4d1d
jodth07 <dimanche.nav@gmail.com>
2019-11-15 08:52:57 -0500

    11/15 - sbt - Added how to install sbt

---
+++ sbt/sbt_install.sh
@@ -0,0 +1,15 @@
+# INSTALL SBT ON UBUNTU/MAC
+
+# download sbt zip
+
+cd
+wget https://piccolo.link/sbt-1.2.7.zip
+
+# unzip sbt
+unzip sbt-1.2.7.zip
+
+# Add sbt to path
+echo PATH=${PATH}:/Users/speedy/sbt/bin >>  ~/.bash_profile
+
+# restart bash file
+source ~/.bash_profile
