2c51b52bc2f526a48f0ecb95f99cdd08f4498c20
Josué D. (JD) <dimanche.nav@gmail.com>
2019-12-06 15:03:34 -0500

    updated sbt installs
    add sbt to /opt instead of under user

--- sbt/sbt_install.sh
+++ sbt/sbt_install.sh
@@ -2,14 +2,17 @@
 
 # download sbt zip
 
-cd
+cd /opt
+mkdir sbt
+cd /opt/sbt
+
 wget https://piccolo.link/sbt-1.2.7.zip
 
 # unzip sbt
 unzip sbt-1.2.7.zip
 
 # Add sbt to path
-echo PATH=${PATH}:/Users/speedy/sbt/bin >>  ~/.bash_profile
+echo PATH=${PATH}:/opt/sbt/bin >>  ~/.bash_profile
 
 # restart bash file
 source ~/.bash_profile
