1954b813cb8683d190e9d827bf391282c2f08e8b
jodth07 <dimanche.nav@gmail.com>
2020-02-02 15:32:36 -0500

    02/02 - sbt - updated sbt path in echo string

--- sbt/sbt_install.sh
+++ sbt/sbt_install.sh
@@ -1,7 +1,7 @@
 # install SBT ON UBUNTU/MAC
 
 # download sbt zip
-cd /home/opt
+cd /opt
 sudo chown $USER .
 wget https://piccolo.link/sbt-1.2.7.zip
 
@@ -9,8 +9,8 @@ wget https://piccolo.link/sbt-1.2.7.zip
 unzip sbt-1.2.7.zip
 
 # set SBT HOME & Add sbt to path
-echo SBT_HOME=/home/opt/sbt  >>  ~/.bash_profile
-echo PATH=${PATH}:$SBT_HOME/bin >>  ~/.bash_profile
+echo SBT_HOME=/opt/sbt  >>  ~/.bash_profile
+echo "export PATH=\${PATH}:\$SBT_HOME/bin" >>  ~/.bash_profile
 
 # restart bash file
 source ~/.bash_profile
