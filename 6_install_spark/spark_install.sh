e2f8691bcf16e192eb4de55612c9c7ce89e96d2e
jodth07 <dimanche.nav@gmail.com>
2020-05-31 20:46:49 -0400

    5/31 - spark | flume - updated pyspark install and flume install

--- 6_install_spark/spark_install.sh
+++ 6_install_spark/spark_install.sh
@@ -1,5 +1,9 @@
 # Download spark
 cd /opt
+sudo chown $USER .
+mkdir downloads
+
+# download spark
 wget http://www.trieuvan.com/apache/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
 
 # extract spark
@@ -18,3 +22,15 @@ export PATH=\$PATH:\$SPARK_HOME/sbin
 # reload  ~/.bash_profile
 source  ~/.bash_profile
 
+
+# set pyspark to use jupyter notebook
+pip install jupyter notebook
+echo"
+
+export PYSPARK_DRIVER_PYTHON=jupyter
+export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
+" >>  ~/.bash_profile
+
+
+# reload  ~/.bash_profile
+source  ~/.bash_profile
--- flume/flume_install.sh
+++ flume/flume_install.sh
@@ -1,19 +1,27 @@
 # make sure java is installed
-# see java_install.md
+
+
+cd /opt
+sudo chown $USER .
+mkdir downloads
 
 # Download and install flume in flume directory
-cd /opt
 wget http://mirror.cogentco.com/pub/apache/flume/1.9.0/apache-flume-1.9.0-bin.tar.gz
 tar -xzvf apache-flume-1.9.0-bin.tar.gz
 ln -s apache-flume-1.9.0-bin flume
+mv apache-flume-1.9.0-bin.tar.gz downloads/
+
 
 # Update `bash_profile`
-nano ~/.bash_profile
+
 
 # add flume path to `.bash_profile`
-echo "# set up FLUME_HOME" >> ~/.bash_profile
-echo export FLUME_HOME=/home/$USER/flume  >> ~/.bash_profile
-echo export PATH=$PATH:$FLUME_HOME/bin  >> ~/.bash_profile
+echo "
+# set up FLUME_HOME
+export FLUME_HOME=/opt/flume
+export PATH=\$PATH:\$FLUME_HOME/bin
+
+" >> ~/.bash_profile
 
 # close `.bash_profile` and run
 source ~/.bash_profile
